c238d472f49452f3c6e4180d15766a8debfc7ade
Wallace Ferancini Rosa <wferancini@gmail.com>
2021-09-08 10:27:35 -0300

    initial url config added

--- api/data_breaches/urls.py
+++ api/data_breaches/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from views import *
+from .views import *
 
 router = routers.DefaultRouter()
-# router.register(r'databreaches', Data)
+router.register(r'databreaches', DataBreachViewSet, basename='databreaches')
 
-urlpatters = [
-    path('', include(router.urls))
+urlpatterns = [
+    path('', include(router.urls)),
 ]

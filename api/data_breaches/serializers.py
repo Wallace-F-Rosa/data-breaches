aa70f5e9a3849aeaaf6c26d3cd4441f97ec4b0cc
Wallace Ferancini Rosa <wferancini@gmail.com>
2021-09-11 20:21:52 -0300

    implementation to create data breach as complex json
    extra data is passed to serializer as context and later the objects
    are created in the serializer create(validated_data) method

--- api/data_breaches/serializers.py
+++ api/data_breaches/serializers.py
@@ -1,14 +1,72 @@
 from rest_framework import serializers
+from django.db import transaction
 from .models import *
 
-class DataBreachSerializer(serializers.ModelSerializer):
+class SourceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = DataBreach
+        model = Source
+        fields = '__all__'
 
 class EntitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Entity
+        fields = '__all__'
 
 class OrganizationTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrganizationType
+        fields = '__all__'
+
+class DataBreachSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataBreach
+        fields = '__all__'
+
+    entity = EntitySerializer()
+
+    def create(self, validated_data):
+        """
+        Override to create Entity, OrganizationType and Source objects
+        using context to pass extra data to serializer. All objects are
+        created in a transaction so either the operation is successfull 
+        or a rollback will be called.
+        """
+        databreach = None
+        with transaction.atomic() :
+            # create entity
+            entity_data = validated_data.pop('entity')
+            entity_serializer = EntitySerializer(data={
+                'name' : entity_data['name']
+            })
+            if not entity_serializer.is_valid():
+                raise serializers.ValidationError(entity_serializer.errors)
+            entity = entity_serializer.save()
+
+            # create organization type
+            org_data = self.context['request'].data['entity']['organization_type']
+            orgs = []
+            for org in org_data:
+                orgs.append({
+                    'organization_type' : org,
+                    'entity' : entity.id
+                })
+            org_serializer = OrganizationTypeSerializer(data=orgs, many=True)
+            if not org_serializer.is_valid():
+                raise serializers.ValidationError(org_serializer.errors)
+
+            # create databreach object
+            databreach = DataBreach.objects.create(**validated_data, entity=entity)
+
+            # create sources
+            source_data = self.context['request'].data['sources']
+            sources = []
+            for source in source_data:
+                sources.append({
+                    'url' : source,
+                    'data_breach' : databreach.id
+                })
+            source_serializer = SourceSerializer(data=sources, many=True)
+            if not source_serializer.is_valid():
+                raise serializers.ValidationError(source_serializer.errors)
+
+        return databreach
--- api/data_breaches/views.py
+++ api/data_breaches/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
-from rest_framework import views
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class DataBreachViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet with CRUD and list functions from data breaches.
+    """
+    queryset = DataBreach.objects.all()
+    serializer_class = DataBreachSerializer
+
+    def get_serializer_context(self):
+        """
+        Pass extra content to DataBreachSerializer using request context.
+        """
+        context = super(DataBreachViewSet, self).get_serializer_context()
+        context.update({
+            "request" : self.request
+        })
+        return context

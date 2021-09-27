ff79b872f11ebc8fad7df22b36a25e20a10ebdab
Wallace Ferancini Rosa <wferancini@gmail.com>
2021-09-27 11:00:46 -0300

    EntitySerializer deals with entity data
    Doesn't make sense to make DataBreachSerializer to deal with entity
    data and this guarantees that we don't have the same entity data being
    handled in different ways across different methods.

--- api/data_breaches/serializers.py
+++ api/data_breaches/serializers.py
@@ -7,16 +7,90 @@ class SourceSerializer(serializers.ModelSerializer):
         model = Source
         fields = '__all__'
 
+class OrganizationTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrganizationType
+        fields = '__all__'
+
 class EntitySerializer(serializers.ModelSerializer):
     name = serializers.CharField()
     class Meta:
         model = Entity
         fields = '__all__'
 
-class OrganizationTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrganizationType
-        fields = '__all__'
+    def to_representation(self, obj):
+        """Override to return correct representation of Entity object.
+        Example:
+
+        .. code-block:: json
+
+            {
+                "id" : 1,
+                "name" : "Test",
+                "organization_type" : ["web"]
+            }
+        """
+
+        org_type = OrganizationType.objects.filter(entity=obj)
+
+        return {
+            'id': obj.id,
+            'name': obj.name,
+            'organization_type': [t.organization_type for t in org_type]
+        }
+
+    def create(self, validated_data):
+        """Override to create Entity and OrganizationType objects with extra content
+        passed in context.
+        Extra data:
+            * organization_type (list) : list of strings containing the organization sphere
+        of action.
+        """
+        with transaction.atomic():
+            entity, created = Entity.objects.get_or_create(name=validated_data['name'])
+            if created:
+                org_data = self.context['extra'].get('organization_type', [])
+                orgs = []
+                for org in org_data:
+                    orgs.append({
+                        'entity' : entity.id,
+                        'organization_type' : org
+                    })
+
+                org_serializer = OrganizationTypeSerializer(data=orgs, many=True)
+                org_serializer.is_valid(raise_exception=True)
+                org_serializer.save()
+
+        return entity
+
+    def update(self, instance, validated_data):
+        """Override to update Entity and OrganizationType objects with extra content
+        passed in context.
+        Extra data:
+            * organization_type (list) : list of strings containing the organization sphere
+        of action.
+        """
+        with transaction.atomic():
+            org_data = self.context['extra'].get('organization_type', [])
+            # substitute the organization_type 
+            if len(org_data) != 0:
+                orgs_qs = OrganizationType.objects.filter(entity=instance)
+                for org in orgs_qs:
+                    org.delete()
+
+                orgs = []
+                for org in org_data:
+                    orgs.append({
+                        'entity' : instance.id,
+                        'organization_type' : org.organization_type
+                    })
+                orgs_serializer = OrganizationTypeSerializer(data=orgs, many=True)
+                orgs_serializer.is_valid(raise_exception=True)
+                orgs_serializer.save()
+
+            instance.name = validated_data.get('name', instance.name)
+            instance.save()
+        return instance
 
 class DataBreachSerializer(serializers.ModelSerializer):
     class Meta:
@@ -96,30 +170,29 @@ class DataBreachSerializer(serializers.ModelSerializer):
 
         """
         databreach = None
-        with transaction.atomic() :
+        validated_data.pop('entity')
+        with transaction.atomic():
             # create entity
-            entity_data = validated_data.pop('entity')
+            entity_data = self.context['extra'].get('entity', {})
 
             # create organization type
-            entity, created = Entity.objects.get_or_create(name=entity_data['name'])
-            if created:
-                org_data = self.context['request'].data['entity']['organization_type']
-                orgs = []
-                for org in org_data:
-                    orgs.append({
-                        'organization_type' : org,
-                        'entity' : entity.id
-                    })
-                org_serializer = OrganizationTypeSerializer(data=orgs, many=True)
-                if not org_serializer.is_valid():
-                    raise serializers.ValidationError(org_serializer.errors)
-                org_serializer.save()
+            entity_serializer = EntitySerializer(
+                data={
+                    'name' : entity_data.get('name', '')
+                }, 
+                context={
+                    'extra' : {
+                        'organization_type' : entity_data.get('organization_type', [])
+                }
+            })
+            entity_serializer.is_valid(raise_exception=True)
+            entity = entity_serializer.save()
 
             # create databreach object
             databreach = DataBreach.objects.create(**validated_data, entity=entity)
 
             # create sources
-            source_data = self.context['request'].data['sources']
+            source_data = self.context['extra'].get('sources', [])
             sources = []
             for source in source_data:
                 sources.append({
@@ -131,3 +204,30 @@ class DataBreachSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError(source_serializer.errors)
             source_serializer.save()
         return databreach
+
+    def update(self, instance, validated_data):
+        """Override to update a DataBreach object. Needs to receive the id
+        of the instance. that needs to be updated.
+        """
+        with transaction.atomic():
+            request_data = self.context['request'].data
+            instance.year = request_data.get('year', instance.year)
+            instance.records = request_data.get('records', instance.records)
+            instance.method = request_data.get('method', instance.method)
+
+            entity = instance.entity
+            if 'entity' in request_data:
+                entity_serializer = EntitySerializer(
+                    data={
+                        'name' : request_data['entity']['name']
+                    }, 
+                    context={
+                        'extra' : {
+                            'organization_type' : request_data['entity']['organization_type']
+                    }
+                })
+                entity_serializer.is_valid(raise_exception=True)
+                instance.entity = entity_serializer.save()
+                instance.save()
+
+        return instance

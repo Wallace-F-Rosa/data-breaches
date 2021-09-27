0a55140b72c359ea3d40b73504730e65bf591ac3
Wallace Ferancini Rosa <wferancini@gmail.com>
2021-09-27 10:55:21 -0300

    fix /data_breaches key error on extra content
    Some data on entity can be optional (organization type data is not
    required to register entity) and sources data may not be included on
    updating data breach data.

--- api/data_breaches/views.py
+++ api/data_breaches/views.py
@@ -7,16 +7,49 @@ from .serializers import *
 class DataBreachViewSet(viewsets.ModelViewSet):
     """
     ViewSet with CRUD and list functions from data breaches.
+
+    To create a data breach you need to pass what entity is involved
+    (organization_type is an option field describing the organization's sphere
+    of action), the year of the breach, the amount of data records breached,
+    the method used and the sources abourding the breach. 
+
+    Example:
+
+    ```json
+    {
+            "entity": {
+                "name": "21st Century Oncology",
+                "organization_type": [
+                    "healthcare"
+                ]
+            },
+            "year": "2016",
+            "records": 2200000,
+            "method": "hacked",
+            "sources": [
+                "https://gizmodo.com/mother-of-all-breaches-exposes-773-million-emails-21-m-1831833456",
+                "http://cbs12.com/news/local/21st-century-oncology-notifies-22-million-of-hacking-data-breach"
+            ]
+    }
+    ```
     """
     queryset = DataBreach.objects.all()
     serializer_class = DataBreachSerializer
 
     def get_serializer_context(self):
-        """
-        Pass extra content to DataBreachSerializer using request context.
+        """Pass extra content to DataBreachSerializer using request context.
+        The data is passed as 'extra' dict.
         """
         context = super(DataBreachViewSet, self).get_serializer_context()
+        entity_data = self.request.data.get('entity', None)
+        extra_data = {
+        }
+        if entity_data:
+            extra_data['entity'] = entity_data
+        sources_data = self.request.data.get('sources', None)
+        if sources_data:
+            extra_data['sources'] = sources_data
         context.update({
-            "request" : self.request
+            'extra' : extra_data
         })
         return context
